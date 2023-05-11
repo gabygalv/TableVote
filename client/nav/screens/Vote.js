592c2d9acbb3a7032065b3284e9bceafda3123b0
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-05-11 16:15:45 -0700

    added pull to refresh on vote tab

--- client/nav/screens/Vote.js
+++ client/nav/screens/Vote.js
@@ -1,26 +1,46 @@
 import React, { useContext, useState } from 'react';
 import UserContext from '../../UserContext.js';
 import RestaurantList from '../../components/restaurantList.js';
-import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar, ScrollView, RefreshControl } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import PartyList from '../../components/partylist.js';
 import SelectWinner from '../../components/selectWinner.js';
-import {useNavigation} from '@react-navigation/native'
-
+import { useNavigation } from '@react-navigation/native'
 
 export default function Vote({ }) {
-  const { isLoggedIn, loggedInParties, setLoggedInParties} = useContext(UserContext);
-  const navigation = useNavigation()
+  const { isLoggedIn, loggedInParties, setLoggedInParties, setRefresh, refresh } = useContext(UserContext);
+  const navigation = useNavigation();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefresh(!refresh)
+    setRefreshing(true);
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000);
+  };
+
   return (
-    <View>
-      <PartyList 
-      setLoggedInParties={setLoggedInParties} 
-      loggedInParties={loggedInParties} 
-      navigation={navigation} 
-      isLoggedIn={isLoggedIn}
+    <ScrollView
+      contentContainerStyle={styles.container}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <PartyList
+        setLoggedInParties={setLoggedInParties}
+        loggedInParties={loggedInParties}
+        navigation={navigation}
+        isLoggedIn={isLoggedIn}
       />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
-  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+});
